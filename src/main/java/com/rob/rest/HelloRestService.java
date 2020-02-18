6b1053a5378cb7daaf912f194124d2c55986af66
Thein Win <thtay.poly@gmail.com>
2020-02-17 21:02:20 -0500

    Initial Push

---
+++ src/main/java/com/rob/rest/HelloRestService.java
@@ -0,0 +1,13 @@
+package com.rob.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+public class HelloRestService {
+    @GET //This annotation indicates GET Requests
+    @Path("/hello")
+    public Response hello() {
+        return Response.status(200).entity("hello").build();
+    }
+}
